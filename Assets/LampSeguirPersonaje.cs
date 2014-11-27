bb2bc5dc2ac5f2808be58ddb56bb9a19e9fd5cf3
Erick Subero <dracobk201@gmail.com>
2014-11-26 23:23:02 -0430

    No se si llamarlo mejoras
    -Foco de luz
    -Linterna, pero aun falta pulirla
    -Manejador de Colisiones que aun no funciona

---
+++ Assets/LampSeguirPersonaje.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LampSeguirPersonaje : MonoBehaviour {
+	
+	public Transform personaje;
+	public float separacionx = 0f;
+	public float separaciony = -16.84f;
+	public float separacionz = 0f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(personaje.position.x+separacionx, personaje.position.y-separaciony, personaje.position.z+separacionz);
+	}
+}
