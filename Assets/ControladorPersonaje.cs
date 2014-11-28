65712fe0a9026adae7df4d92f01282af7c1bee15
Erick Subero <dracobk201@gmail.com>
2014-11-28 13:42:29 -0430

    Cámara funcionando
    La cámara ya funciona, agregue el fondo pero no doy con las colisiones
    del jugador.

--- Assets/ControladorPersonaje.cs
+++ Assets/ControladorPersonaje.cs
@@ -25,26 +25,26 @@ public class ControladorPersonaje : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey(KeyCode.A)) {
-			transform.Translate(-Vector2.right  * speed * Time.deltaTime);
+			transform.Translate(-Vector3.right  * speed * Time.deltaTime);
 			izquierda = true;
 			//Debug.Log (izquierda);
 			LimpiandoVariables("izquierda");
 		}
 		
 		if(Input.GetKey(KeyCode.D)) {
-			transform.Translate(Vector2.right * speed * Time.deltaTime);
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
 			derecha = true;
 			LimpiandoVariables("derecha");
 		}
 
 		if(Input.GetKey(KeyCode.W)) {
-			transform.Translate(Vector2.up * speed * Time.deltaTime);
+			transform.Translate(Vector3.up * speed * Time.deltaTime);
 			subiendo = true;
 			LimpiandoVariables("subiendo");
 		}
 
 		if(Input.GetKey(KeyCode.S)) {
-			transform.Translate(-Vector2.up * speed * Time.deltaTime);
+			transform.Translate(-Vector3.up * speed * Time.deltaTime);
 			bajando = true;
 			LimpiandoVariables("bajando");
 		}
--- Assets/SeguirPersonaje.cs
+++ Assets/SeguirPersonaje.cs
@@ -8,6 +8,6 @@ public class SeguirPersonaje : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(personaje.position.x+separacion, personaje.position.y+separacion, transform.position.z);
+		transform.position = new Vector3(personaje.position.x+separacion, transform.position.y+separacion, personaje.position.z+separacion);
 	}
 }
